4f77a2120764618f3d227526c79471761512fdb7
Federico Monfasani <67231410+fmonfasani@users.noreply.github.com>
2025-07-24 02:26:47 -0300

    feat(front): use env API url

--- front/src/api/auth.js
+++ front/src/api/auth.js
@@ -1,4 +1,6 @@
-const API_URL = "http://localhost:8000/auth";
+// Base URL for the authentication endpoints
+// Defaults to localhost for development if the env variable is not provided
+const API_URL = `${process.env.REACT_APP_API_URL || 'http://localhost:8000'}/auth`;
 
 export async function loginRequest(email, password) {
   const res = await fetch(`${API_URL}/signin`, {
